ae5dac1f0a96ad6d07d071d6ca1a9983a58e332a
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-17 12:27:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object,key,val){
   var newObj = Object.assign({}, object, { [key]: val });
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({}, object, { [key]: val});
+}
